4bcc1a3785eff0dd6f134a3223072f9c4c486275
Haseeb1256 <160749566+Haseeb1256@users.noreply.github.com>
2024-03-21 23:31:43 +0500

    Add files via upload

---
+++ abbi.js
@@ -0,0 +1,6 @@
+var a = 5;
+var b = 2;
+a++; // a become 6
+b--; // b become 1
+var result = 5 + 2 + a++ + b--;
+console.log(result);
---
+++ abbi.ts
@@ -0,0 +1,16 @@
+let a=5
+let b=2
+a++; // a become 6
+b--; // b become 1
+
+
+let result = 5 + 2 + a++ + b--
+console.log (result);
+
+
+
+
+
+
+
+
